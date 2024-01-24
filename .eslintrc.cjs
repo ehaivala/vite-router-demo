c5cd71f468f9a7780b5af01581cb59707dd5d4b2
Eetu Häivälä <eetu.haivala@gmail.com>
2024-01-24 19:32:55 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 0, // 0 = off, 1 = warn, 2 = error
+  },
+  settings: {
+    react: {
+      version: '18',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+};
